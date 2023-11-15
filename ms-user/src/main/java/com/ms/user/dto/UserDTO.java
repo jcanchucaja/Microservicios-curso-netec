8a92194b68d474822cd17d70d3ae0c3a4bf380e1
jcanchucaja <jcanchucaja@yahoo.com.mx>
2023-11-14 19:39:22 -0500

    Subida

---
+++ ms-user/src/main/java/com/ms/user/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package com.ms.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private String document;
+
+    private String name;
+
+    private String email;
+
+    private String information;
+}
